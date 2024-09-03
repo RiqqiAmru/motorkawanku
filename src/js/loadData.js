101e8fcd5c53d5d91c90bd88d2d36fec1c34b58a
RiqqiAmru <Riqqi0069@stmik-wp.ac.id>
2024-09-03 17:57:29 +0700

    make a fake api to kumuh kawasan, rt, investasi

--- src/js/loadData.js
+++ src/js/loadData.js
@@ -15,18 +15,18 @@ wb = read(b64, { type: "base64" });
 wsname = wb.SheetNames[2];
 let rtrw = utils.sheet_to_json(wb.Sheets[wsname]);
 
-// wb = read(b64, { type: "base64" });
-// wsname = wb.SheetNames[3];
-// let kumuhKawasan = utils.sheet_to_json(wb.Sheets[wsname]);
+wb = read(b64, { type: "base64" });
+wsname = wb.SheetNames[3];
+let kumuhKawasan = utils.sheet_to_json(wb.Sheets[wsname]);
 
-// // read data kumuhRT
-// wb = read(b64, { type: "base64" });
-// wsname = wb.SheetNames[4];
-// let kumuhRT = utils.sheet_to_json(wb.Sheets[wsname]);
+// read data kumuhRT
+wb = read(b64, { type: "base64" });
+wsname = wb.SheetNames[4];
+let kumuhRT = utils.sheet_to_json(wb.Sheets[wsname]);
 
-// wb = read(b64, { type: "base64" });
-// wsname = wb.SheetNames[5];
-// let semuaInvestasi = utils.sheet_to_json(wb.Sheets[wsname]);
+wb = read(b64, { type: "base64" });
+wsname = wb.SheetNames[5];
+let semuaInvestasi = utils.sheet_to_json(wb.Sheets[wsname]);
 
 // const kegiatanInvestasi = aspek.kegiatanInvestasi;
 // const aspekKumuh = aspek.aspek;
@@ -51,9 +51,9 @@ async function fakeFetch(param = false, id = 0, tahun = 0) {
     });
     return Promise.resolve({ ...kota, kec });
   } else {
-    // pecah url berdasarkan /
     // url/kawasan/id_kawasan/tahun
     if (tahun == 0 && param == "kawasan" && id) {
+      // header kawasan
       const kec = kecamatan.find((k) => k.id == id);
       const rt = rtrw
         .filter((rt) => rt.kawasan == id)
@@ -62,10 +62,26 @@ async function fakeFetch(param = false, id = 0, tahun = 0) {
         });
       return { ...kec, rt };
     } else if (tahun == 0 && id && param == "rt") {
+      // header rt
       const rt = rtrw.find((r) => r.id == id);
       return rt;
+    } else if (id && (param == "kawasan") & (tahun != 0)) {
+      // fetch tabel kawasan
+      const kumuhK = kumuhKawasan.find((k) => k.id == id);
+      return kumuhK;
+    } else if (id && param == "rt" && tahun != 0) {
+      // fetch tabel rt
+      const kumuhK = kumuhRT.find((k) => k.id == id);
+      return kumuhK;
+    } else if (id && (param == "investasiKawasan") & (tahun != 0)) {
+      // investasi kawasan
+      const inv = semuaInvestasi.filter(
+        (sin) => sin.idKawasan == id && sin.tahun == tahun
+      );
+      return inv;
     }
-    // fetch tabel kawasan
+
+    // investasi rt
   }
   return Promise.reject("undefined");
 }
--- src/test/fakeApi.test.js
+++ src/test/fakeApi.test.js
@@ -39,3 +39,150 @@ test("api for header rt", async () => {
   expect(await fakeFetch("rt", 1)).toHaveProperty("panjangJalanIdeal");
   expect(await fakeFetch("rt", 1)).toHaveProperty("panjangDrainaseIdeal");
 });
+
+test("api for kumuh kawasan", async () => {
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("id");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("kawasan");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("tahun");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("1av");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("1ap");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("1an");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("1bv");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("1bp");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("1bn");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("1cv");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("1cp");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("1cn");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("1r");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("2av");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("2ap");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("2an");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("2bv");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("2bp");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("2bn");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("2r");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("3av");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("3ap");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("3an");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("3bv");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("3bp");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("3bn");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("3r");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("4av");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("4ap");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("4an");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("4bv");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("4bp");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("4bn");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("4cv");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("4cp");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("4cn");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("4r");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("5av");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("5ap");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("5an");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("5bv");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("5bp");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("5bn");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("5r");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("6av");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("6ap");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("6an");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("6bv");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("6bp");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("6bn");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("6r");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("7av");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("7ap");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("7an");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("7bv");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("7bp");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("7bn");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("7r");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("totalNilai");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty(
+    "tingkatKekumuhan"
+  );
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty(
+    "ratarataKekumuhan"
+  );
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty(
+    "kontribusiPenanganan"
+  );
+});
+
+test("api for kumuh rt", async () => {
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("id");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("kawasan");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("tahun");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("tahun");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("1av");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("1ap");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("1an");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("1bv");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("1bp");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("1bn");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("1cv");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("1cp");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("1cn");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("1r");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("2av");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("2ap");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("2an");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("2bv");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("2bp");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("2bn");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("2r");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("3av");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("3ap");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("3an");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("3bv");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("3bp");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("3bn");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("3r");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("4av");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("4ap");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("4an");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("4bv");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("4bp");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("4bn");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("4cv");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("4cp");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("4cn");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("4r");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("5av");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("5ap");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("5an");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("5bv");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("5bp");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("5bn");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("5r");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("6av");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("6ap");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("6an");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("6bv");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("6bp");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("6bn");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("6r");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("7av");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("7ap");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("7an");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("7bv");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("7bp");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("7bn");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("7r");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty("totalNilai");
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty(
+    "tingkatKekumuhan"
+  );
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty(
+    "ratarataKekumuhan"
+  );
+  expect(await fakeFetch("kawasan", 1, 2023)).toHaveProperty(
+    "kontribusiPenanganan"
+  );
+});
+
+test("api investasi kawasan", async () => {
+  expect(await fakeFetch("investasiKawasan", 1, 2024)).toHaveProperty("id");
+});
